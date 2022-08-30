1b7a1808d3b2642d24f51791b80239572aec7d65
jgbpessoa <jgbpessoa@gmail.com>
2022-08-30 16:53:21 -0500

    getByLabelText

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -16,6 +16,9 @@ describe("Application", () => {
     });
     expect(nameElement).toBeInTheDocument();
 
+    const nameElement2 = screen.getByLabelText("Name");
+    expect(nameElement2).toBeInTheDocument();
+
     const bioElement = screen.getByRole("textbox", {
       name: "Bio",
     });
@@ -26,6 +29,11 @@ describe("Application", () => {
 
     const termsElement = screen.getByRole("checkbox");
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
 
     const submitButtonElement = screen.getByRole("button");
     expect(submitButtonElement).toBeInTheDocument();
